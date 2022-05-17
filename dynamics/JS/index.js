e05758cfacfb1fed8aefae66a56a5dd0f7291f22
JoseAntonioZarcoRomero <zarco_romero_jose_antonio@comunidad.unam.mx>
2022-05-17 00:15:24 -0500

    Código comentado

--- dynamics/JS/index.js
+++ dynamics/JS/index.js
@@ -5,66 +5,129 @@ const buscaminas = {
     ncolumnas: 8,
     mCampo: []
 };
+/*Pinta el tablero
+------------------------------------------------------------------------------------------*/
+function pintarTablero(){
+    //Obtenemos del HTML el objeto tablero
+    const tablero = document.querySelector("#tablero");
 
+    //Actualiza variables del CSS
+    document.querySelector("html").style.setProperty("--nfilas",buscaminas.nfilas);
+    document.querySelector("html").style.setProperty("--ncolumnas",buscaminas.ncolumnas);
+
+    //En caso de existir, borra el tablero actual
+    while (tablero.firstChild) {
+        tablero.firstChild.removeEventListener("click", color);
+        tablero.firstChild.removeEventListener("click", abrir);
+        tablero.firstChild.removeEventListener("contextmenu", bandera);
+        tablero.removeChild(tablero.firstChild);
+    }
+
+    //Crea las casillas (a partir de las filas y columnas del objeto)
+    for(let fila=0; fila<buscaminas.nfilas; fila++){
+        for(let columna=0; columna<buscaminas.ncolumnas; columna++){
+            //Creación del elemento
+            const casilla = document.createElement("div");
+
+            //Establece id al elemento
+            casilla.setAttribute("id","fila" + fila + "_columna" + columna );
+
+            //Evento pasar el mouse encima del elemento
+            casilla.addEventListener("mouseover", color);
+            
+            //Evento clic izquierdo
+            casilla.addEventListener("click", abrir);
+
+            //Evento clic derecho
+            casilla.addEventListener("contextmenu", bandera);
+
+            //Agrega el elemento al final de la lista
+            tablero.appendChild(casilla);
+        }
+    }
+}
+/*Crea matriz para el tablero
+------------------------------------------------------------------------------------------*/
+//Genera matriz sin minas
+function generarCampoVacio(){
+    buscaminas.mCampo = new Array(buscaminas.nfilas);//crea arreglo según el #filas
+    for (let fila=0; fila<buscaminas.nfilas; fila++){
+        //crea un arreglo en cada índice del arreglo anterior, según el #columnas
+        buscaminas.mCampo[fila] = new Array(buscaminas.ncolumnas);
+    }
+}
+//Genera numeros aleatorios dentro del rango min-max
 function nrand(min, max){
     return Math.floor(Math.random()*(max-min)+min);
 }
-
-function generarCampoVacio(){
-    buscaminas.mCampo = new Array(buscaminas.nfilas);
-    for (let fila=0; fila<buscaminas.nfilas; fila++){
-        buscaminas.mCampo[fila] = new Array(buscaminas.ncolumnas);
-    }
-    //console.log(buscaminas.mCampo);
-}
-
+//Coloca minas aleatoriamente en la matriz del tablero
 function colocarMinas(){
     let mColocadas = 0;
+    //Se ejecuta mientras el numero de minas colocadas se menor que el de las totales
     while(mColocadas<buscaminas.mTotales){
+        //No. aleatorio para fila y columna
         let filaRand = nrand(0,buscaminas.nfilas);
         let columnaRand = nrand(0,buscaminas.ncolumnas);
+        //Si en esa posición no existe una mina, coloca una
         if(buscaminas.mCampo[filaRand][columnaRand] != "M"){
             buscaminas.mCampo[filaRand][columnaRand] = "M";
             mColocadas++;
         }
     }
 }
-
-function contarMinasAlrededorCasilla(fila,columna){
+//Cuenta el #minas que exiten alrededor de la casilla
+function mAlrededor(fila,columna){
     let mAlrededor = 0;
-    let fAnterior=fila-1, fPosterior=fila+1;
-    let cAnterior=columna-1, cPosterior=columna+1;
-    for(fAnterior; fAnterior<=fila; fPosterior++){
-        for(cAnterior; cAnterior<=fila; cPosterior++){
-            if(buscaminas.mCampo[fAnterior][cAnterior]){
-                mAlrededor++;
+    //De la fila anterior a la posterior
+    for (let zFila = fila-1; zFila <= fila+1; zFila++){
+        //De la columna anterior a la posterior
+        for (let zColumna = columna-1; zColumna <= columna+1; zColumna++){
+            //Verifica que la casilla este dentro del tablero
+            if(zFila>-1 && zFila<buscaminas.nfilas && zColumna>-1 && zColumna<buscaminas.ncolumnas){
+                if(buscaminas.mCampo[zFila][zColumna] == "M"){
+                    mAlrededor++;
+                }
             }
         }
     }
     buscaminas.mCampo[fila][columna] = mAlrededor;
 }
-
-
+//Recorre toda la matriz
+function contarMinas(){
+    for (let fila=0; fila<buscaminas.nfilas; fila++){
+        for (let columna=0; columna<buscaminas.ncolumnas; columna++){
+            //Si la casilla no tiene una mina
+            if (buscaminas.mCampo[fila][columna] != "M"){
+                mAlrededor(fila,columna);
+            }
+        }
+    }
+}
+/*Funciones a eventos
+------------------------------------------------------------------------------------------*/
+function color(evento){
+    // console.log(evento.target);
+    // evento.target.style.backgroundcolor = "red";
+}
 function bandera(evento){
-    alert("bandera");
+    // console.log(evento);
+    // let casilla = evento.target;
+    // console.log(casilla);
+
+    //evento.target
+
+    evento.stopPropagation();//Detiene bubbling
+    evento.preventDefault();//No mostrar menú del navegador
 }
 function abrir (evento){
     alert("abrir");
 }
-function pintarTablero(){
-    const tablero = document.querySelector("#tablero");
-    while (tablero.firstChild) {
-        tablero.removeChild(tablero.firstChild);
-    }
-    document.querySelector("html").style.setProperty("--nfilas",buscaminas.nfilas);
-    document.querySelector("html").style.setProperty("--ncolumnas",buscaminas.ncolumnas);
-    for(let fila=0; fila<buscaminas.nfilas; fila++){
-        for(let columna=0; columna<buscaminas.ncolumnas; columna++){
-            const casilla = document.createElement("div");
-            casilla.setAttribute("id","fila" + fila + "_columna" + columna );
-            casilla.addEventListener("contextmenu", bandera);
-            casilla.addEventListener("click", abrir);
-            tablero.appendChild(casilla);
-        }
-    }
+//Por mientras
+function inicio(){
+    pintarTablero();
+    generarCampoVacio();
+    colocarMinas();
+    contarMinas();
+    console.log(buscaminas.mCampo);
 }
+window.onload = inicio;
--- dynamics/puntajes.php
+++ dynamics/puntajes.php
@@ -8,15 +8,15 @@
     </head>
     <body>
         <?php
-        echo "<h1>Mejores tiempos</h1>";
-        echo "<h2>Tablero fácil</h2>";
-        echo "<h2>Tablero medio</h2>";
-        echo "<h2>Tablero difícil</h2>";
-        echo "
-        <form action='../index.html' method='post' target='_self'>
-            <button>Regresar al menú</button>
-        </form>
-        ";
+            echo "<h1>Mejores tiempos</h1>";
+            echo "<h2>Tablero fácil</h2>";
+            echo "<h2>Tablero medio</h2>";
+            echo "<h2>Tablero difícil</h2>";
+            echo "
+                <form action='../index.html' method='post' target='_self'>
+                    <button>Regresar al menú</button>
+                </form>
+            ";
         ?>
     </body>
 </html>
