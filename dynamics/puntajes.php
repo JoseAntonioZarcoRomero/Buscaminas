9008db4b503a837a7cb9136d515288be5b326e7c
JoseAntonioZarcoRomero <zarco_romero_jose_antonio@comunidad.unam.mx>
2022-05-17 21:12:12 -0500

    a

--- dynamics/puntajes.php
+++ dynamics/puntajes.php
@@ -1,6 +1,32 @@
 <?php
     echo "<h1>Mejores tiempos</h1>";
     echo "<h2>Tablero fácil</h2>";
+    echo "
+        <table align='center' border='1' cellpadding=10px>
+            <thead>
+                <tr>
+                    <th>Lugar</th>
+                    <th>Jugador</th>
+                    <th>Tiempo para acabar</th>
+                    <th>Fecha</th>
+                </tr>
+            </thead>
+            <tbody>";
+    
+    echo "
+                <tr>
+                    <td>
+                    </td>
+                    <td>
+                    </td>
+                    <td>
+                    </td>
+                    <td>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    ";
     echo "<h2>Tablero medio</h2>";
     echo "<h2>Tablero difícil</h2>";
     echo "
