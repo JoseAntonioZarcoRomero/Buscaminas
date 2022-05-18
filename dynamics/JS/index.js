90d4c3eb71ed244e559bdeb5bff5d61170ac6dbc
JoseAntonioZarcoRomero <zarco_romero_jose_antonio@comunidad.unam.mx>
2022-05-18 03:29:58 -0500

    Lógica de buscaminas terminada?

--- dynamics/JS/index.js
+++ dynamics/JS/index.js
@@ -4,54 +4,40 @@ const buscaminas = {
     nfilas: 8,
     ncolumnas: 8,
     mCampo: []
-};
+}
 /*Pinta el tablero
 ------------------------------------------------------------------------------------------*/
 function pintarTablero(){
     //Obtenemos del HTML el objeto tablero
-    const tablero = document.querySelector("#tablero");
-
+    let tablero = document.querySelector("#tablero");
     //Actualiza variables del CSS
     document.querySelector("html").style.setProperty("--nfilas",buscaminas.nfilas);
     document.querySelector("html").style.setProperty("--ncolumnas",buscaminas.ncolumnas);
-
     //En caso de existir, borra el tablero actual
     while (tablero.firstChild) {
-        tablero.firstChild.removeEventListener("click", color);
-        tablero.firstChild.removeEventListener("click", abrir);
-        tablero.firstChild.removeEventListener("contextmenu", bandera);
+        tablero.firstChild.removeEventListener("contextmenu",bandera);
+        tablero.firstChild.removeEventListener("click",abrir);
         tablero.removeChild(tablero.firstChild);
     }
-
     //Crea las casillas (a partir de las filas y columnas del objeto)
-    for(let fila=0; fila<buscaminas.nfilas; fila++){
-        for(let columna=0; columna<buscaminas.ncolumnas; columna++){
-            //Creación del elemento
-            const casilla = document.createElement("div");
-
-            //Establece id al elemento
-            casilla.setAttribute("id", fila + "_" + columna );
-
-            //Evento pasar el mouse encima del elemento
-            casilla.addEventListener("mouseover", color);
-            
-            //Evento clic izquierdo
-            casilla.addEventListener("click", abrir);
-
-            //Evento clic derecho
-            casilla.addEventListener("contextmenu", bandera);
-
-            //Agrega el elemento al final de la lista
-            tablero.appendChild(casilla);
+    for(let f=0; f < buscaminas.nfilas; f++){
+        for(let c=0; c < buscaminas.ncolumnas; c++){
+            let newDiv = document.createElement("div");//Creación del elemento
+            newDiv.setAttribute("id","f" + f + "_c" + c );//Establece id al elemento
+            newDiv.dataset.fila = f;
+            newDiv.dataset.columna = c;
+            newDiv.addEventListener("contextmenu",bandera);
+            newDiv.addEventListener("click",abrir);
+            tablero.appendChild(newDiv);//Agrega el elemento al final de la lista
         }
     }
 }
 /*Crea matriz para el tablero
 ------------------------------------------------------------------------------------------*/
 //Genera matriz sin minas
-function generarCampoVacio(){
+function mMatriz(){
     buscaminas.mCampo = new Array(buscaminas.nfilas);//crea arreglo según el #filas
-    for (let fila=0; fila<buscaminas.nfilas; fila++){
+    for (let fila=0; fila < buscaminas.nfilas; fila++){
         //crea un arreglo en cada índice del arreglo anterior, según el #columnas
         buscaminas.mCampo[fila] = new Array(buscaminas.ncolumnas);
     }
@@ -61,10 +47,9 @@ function nrand(min, max){
     return Math.floor(Math.random()*(max-min)+min);
 }
 //Coloca minas aleatoriamente en la matriz del tablero
-function colocarMinas(){
+function mEsparcidas(){
     let mColocadas = 0;
-    //Se ejecuta mientras el numero de minas colocadas se menor que el de las totales
-    while(mColocadas<buscaminas.mTotales){
+    while (mColocadas < buscaminas.mTotales){
         //No. aleatorio para fila y columna
         let filaRand = nrand(0,buscaminas.nfilas);
         let columnaRand = nrand(0,buscaminas.ncolumnas);
@@ -103,85 +88,138 @@ function contarMinas(){
         }
     }
 }
-/*Funciones a eventos
+/*Funciones de eventos
 ------------------------------------------------------------------------------------------*/
-function color(evento){
-    const casillaClickeada =  evento.target;
-    // evento.target.style.backgroundcolor = "red";
-}
+//Evento clic derecho
 function bandera(evento){
-    const casillaClickeada =  evento.target;
-    console.log(casillaClickeada);
-    evento.stopPropagation();//Detiene bubbling
-    evento.preventDefault();//No mostrar menú del navegador
-    if(casillaClickeada.classList.contains("icon-flag")){
-        casillaClickeada.classList.remove("icon-flag");
-        casillaClickeada.classList.add("icon-duda");
-        buscaminas.mEncontradas--;
-    } else if(casillaClickeada.classList.contains("icon-duda")){
-        casillaClickeada.classList.remove("icon-duda");
-    } else if (casillaClickeada.classList.length == 0){
-        casillaClickeada.classList.add("icon-flag");
-        buscaminas.mEncontradas++;
-        if(buscaminas.mEncontradas == buscaminas.mTotales){
-            buscaminas.resolverTablero(true);
+    if (evento.type === "contextmenu"){
+        let casillaClickeada = evento.currentTarget;//Obtiene el elemento que ha disparado el evento
+        evento.stopPropagation();//Detiene bubbling
+        evento.preventDefault();//No mostrar menú del navegador
+        //Obtiene los datos de las propiedades dataset (string -> número)
+        let fila = parseInt(casillaClickeada.dataset.fila,10);
+        let columna = parseInt(casillaClickeada.dataset.columna,10);
+        //Verifica que la casilla este dentro del tablero
+        if (fila>=0 && columna>=0 && fila< buscaminas.nfilas && columna < buscaminas.ncolumnas) {
+            if (casillaClickeada.classList.contains("flag")){
+               casillaClickeada.classList.remove("flag");
+                buscaminas.mEncontradas--;
+            } else if (casillaClickeada.classList.length == 0){
+                casillaClickeada.classList.add("flag");
+                buscaminas.mEncontradas++;
+                if (buscaminas.mEncontradas == buscaminas.mTotales){
+                    resolverTablero(true);//Resuelve el tablero para ver si esta bien
+                }
+            }
+            mRestantes(); //Actualiza el contador del número de minas restantes
         }
     }
 }
-function abrir (evento){
-    const casillaClickeada =  evento.target;
-    let datos = casillaClickeada.id.split("_",2);
-    let fila = datos[0];
-    let columna = datos[1];
-    abreCasilla(fila,columna);
-    // let id_casilla = "#"+evento.target.id;
-    // let casilla = document.querySelector(id_casilla);
-    // console.log(casilla);
-    // if(!casilla.contains("open")){
-    //     if(!casilla.classList.contains("flag")){
-    //         evento.target.classList.add("open");
-    //         casilla.innerHTML = buscaminas.mCampo[fila][columna];
-    //     }
-    // }
+//Evento clic izquierdo
+function abrir(evento){
+    if (evento.type === "click"){
+        let casilla = evento.currentTarget;//Obtiene el elemento que ha disparado el evento
+        evento.stopPropagation();//Detiene bubbling
+        evento.preventDefault();//No mostrar menú del navegador
+        //Obtiene los datos de las propiedades dataset (string -> número)
+        let fila = parseInt(casilla.dataset.fila,10);
+        let columna = parseInt(casilla.dataset.columna,10);
+        abrirCasilla(fila,columna);
+    }
 }
-function abreCasilla(fila,columna){
-    console.log("Abrimos la casilla de la fila:"+fila+" y columna:"+columna);
-    if (fila>-1 && fila<buscaminas.nfilas && columna>-1 && columna<buscaminas.ncolumnas){
-        //let casilla = document.querySelector("#" + fila + "_" + columna);
-        let casilla = document.getElementById("#"+fila+"_"+columna);
+function abrirCasilla(fila, columna){
+    //Verifica que la casilla este dentro del tablero
+    if (fila >-1 && fila < buscaminas.nfilas && columna >-1 && columna < buscaminas.ncolumnas){
+        //Obtiene la casilla con la fila y columna
+        let casilla = document.querySelector("#f" + fila + "_c" + columna);
         if (!casilla.classList.contains("abierta")){
-            if (!casilla.classList.contains("icon-flag")){
+            if (!casilla.classList.contains("flag")){
                 casilla.classList.add("abierta");
+                //Coloca en la casilla el número de minas que tiene alrededor
                 casilla.innerHTML = buscaminas.mCampo[fila][columna];
+                //Actualiza variables del CSS
+                casilla.classList.add("c" + buscaminas.mCampo[fila][columna])
                 if (buscaminas.mCampo[fila][columna] != "M"){
-                    if (buscaminas.mCampo[fila][columna] == 0){
-                        abreCasilla(fila-1,columna-1);
-                        abreCasilla(fila-1,columna);
-                        abreCasilla(fila-1,columna+1);
-                        abreCasilla(fila,columna-1);
-                        abreCasilla(fila,columna+1);
-                        abreCasilla(fila+1,columna-1);
-                        abreCasilla(fila+1,columna);
-                        abreCasilla(fila+1,columna+1);
-                        casilla.innerHTML  = "";
+                    //Abre las casillas contiguas
+                    if (buscaminas.mCampo[fila][columna] == 0){//Recursivo
+                        abrirCasilla(fila-1,columna-1);
+                        abrirCasilla(fila-1,columna);
+                        abrirCasilla(fila-1,columna+1);
+                        abrirCasilla(fila,columna-1);
+                        abrirCasilla(fila,columna+1);
+                        abrirCasilla(fila+1,columna-1);
+                        abrirCasilla(fila+1,columna);
+                        abrirCasilla(fila+1,columna+1);
                     }
-                } else if(buscaminas.mCampo[fila][columna] == "M"){
+                }else if (buscaminas.mCampo[fila][columna] == "M"){
                     casilla.innerHTML = "";
-                    casilla.classList.add("icon-bomba");
+                    casilla.classList.add("mina");//Diuja mina
                     casilla.classList.add("sinmarcar");
-                    resolverTablero(false);
+                    resolverTablero(false);//Resuelve el tablero indicando que perdimos
                 }
             }
         }
     }
 }
-
-//Por mientras
-function inicio(){
-    pintarTablero();
-    generarCampoVacio();
-    colocarMinas();
-    contarMinas();
-    console.log(buscaminas.mCampo);
+function resolverTablero(ganarOperder){
+    let aCasillas = tablero.children;
+    for (let i = 0 ; i < aCasillas.length; i++){
+        aCasillas[i].removeEventListener("click", abrir);
+        aCasillas[i].removeEventListener("contextmenu", bandera);
+        //Obtiene la casilla con la fila y columna
+        let fila = parseInt(aCasillas[i].dataset.fila,10);
+        let columna = parseInt(aCasillas[i].dataset.columna,10);
+        if (aCasillas[i].classList.contains("flag")){
+            if (buscaminas.mCampo[fila][columna] == "M"){//Bandera correcta
+                aCasillas[i].classList.add("abierta");
+                aCasillas[i].classList.remove("flag");
+                aCasillas[i].classList.add("mina");
+            } else {//Bandera errónea
+                aCasillas[i].classList.add("abierta");
+                aCasillas[i].classList.add("NOflag");
+                ganarOperder = false;
+            }
+        } else if (!aCasillas[i].classList.contains("abierta")){
+            if (buscaminas.mCampo[fila][columna] == "M"){//destapamos el resto de las minas
+                aCasillas[i].classList.add("abierta");
+                aCasillas[i].classList.add("mina");
+            }
+        }
+    }
+    if (ganarOperder == true){
+        alert("Ganaste");
+    } else {
+        alert("Perdiste");
+    }
 }
-window.onload = inicio;
+//Contador del número de minas restantes
+function mRestantes(){
+    document.querySelector("#numMinasRestantes").innerHTML = (buscaminas.mTotales - buscaminas.mEncontradas);
+}
+function inicio(numFilas,numColumnas,totMinas){
+    buscaminas.nfilas = numFilas;
+    buscaminas.ncolumnas = numColumnas;
+    buscaminas.mTotales = totMinas;
+    pintarTablero();
+    mMatriz();
+    mEsparcidas();
+    contarMinas();
+    mRestantes();
+}
+
+const modalidad = document.getElementById("contModalidad");
+const juego = document.getElementById("juego");
+const returnInicio = document.getElementById("btn-returnInicio");
+modalidad.addEventListener("click", (event)=>{
+    const modalidadClickeada = event.target;
+    if(modalidadClickeada.id == 'btn-facil'){
+        inicio(8,8,10);
+    } else if(modalidadClickeada.id == 'btn-medio'){
+        inicio(16,16,40);
+    } else if(modalidadClickeada.id == 'btn-dificil'){
+        inicio(24,24,99);
+    }
+    modalidad.style.display = "none";
+    juego.style.display = "block";
+    returnInicio.style.display = "block";
+});
